9446bd3b5df5c14ccfdcee7d75d2b7e1b637d37b
ivcagide <ivcagide@unirioja.es>
2018-03-01 18:13:23 +0100

    PersistenciaConUsuario

--- JoinSport/app/src/main/java/Persistencia/Persistencia.java
+++ JoinSport/app/src/main/java/Persistencia/Persistencia.java
@@ -2,24 +2,40 @@ package Persistencia;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-public static class Persistencia {
+public class Persistencia {
 
-    public Connection conectar() {
+    public static Connection conectar() {
         Connection conexionMySQL = null;
-        Class.forName("com.mysql.jdbc.Driver").newInstance ();
-        conexionMySQL = DriverManager.getConnection("jdbc:mysql://99.000webhost.io:3306/id4898649", "root", "Jsport2018");
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conexionMySQL = DriverManager.getConnection("jdbc:mysql://99.000webhost.io:3306/id4898649", "root", "Jsport2018");
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return conexionMySQL;
     }
 
     //MÉTODOS DE USUARIO
-    public void insertarUsuario(String nombre, String apellidos, String password, String email, int telefono,
+    public static void insertarUsuario(String nombre, String apellidos, String password, String email, int telefono,
                                 String sexo, String ciudad, String provincia, String foto) {
         Connection con=conectar();
         String consulta="INSERT INTO Usuario(NOMBRE, APELLIDOS, PASSWORD, EMAIL, TELEFONO, SEXO, CIUDAD, PROVINCIA, FOTO) " +
                         " values(?,?,?,?,?,?,?,?,?)";
-        PreparedStatement ps=con.prepareStatement(consulta);
-        ps.setString(1, nombre);
-        ps.set
+        try {
+            PreparedStatement ps=con.prepareStatement(consulta);
+            ps.setString(1, nombre);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
