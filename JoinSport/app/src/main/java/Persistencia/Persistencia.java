d0519c9310616bdb6afa091a2615335f9f9f40d4
UNIRIOJA\paezqua <paulaezquerro@hotmail.com>
2018-03-01 18:31:55 +0100

    Revert "dsfh"
    This reverts commit d5a3aa7e23ed19f80b36bdcb6e8c81100537705a.

--- JoinSport/app/src/main/java/Persistencia/Persistencia.java
+++ JoinSport/app/src/main/java/Persistencia/Persistencia.java
@@ -4,49 +4,61 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Date;
 
-public class Persistencia {
+public static class Persistencia {
 
-    public static Connection conectar() {
+    public Connection conectar() {
         Connection conexionMySQL = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            conexionMySQL = DriverManager.getConnection("jdbc:mysql://99.000webhost.io:3306/id4898649", "root", "Jsport2018");
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return conexionMySQL;
+        Class.forName("com.mysql.jdbc.Driver").newInstance ();
+        conexionMySQL = DriverManager.getConnection("jdbc:mysql://99.000webhost.io:3306/id4898649", "root", "Jsport2018");
     }
 
     //MÉTODOS DE USUARIO
-    public static void insertarUsuario(String nombre, String apellidos, String password, String email, int telefono,
+    public void insertarUsuario(String nombre, String apellidos, String password, String email, int telefono,
                                 String sexo, String ciudad, String provincia, String foto) {
         Connection con=conectar();
         String consulta="INSERT INTO Usuario(NOMBRE, APELLIDOS, PASSWORD, EMAIL, TELEFONO, SEXO, CIUDAD, PROVINCIA, FOTO) " +
                         " values(?,?,?,?,?,?,?,?,?)";
-        try {
-            PreparedStatement ps=con.prepareStatement(consulta);
-            ps.setString(1, nombre);
-            ps.setString(2, apellidos);
-            ps.setString(3, password);
-            ps.setString(4, email);
-            ps.setInt(5, telefono);
-            ps.setString(6 , sexo);
-            ps.setString(7 , ciudad);
-            ps.setString(8 , provincia);
-            ps.setString(9 , foto);
+        PreparedStatement ps=con.prepareStatement(consulta);
+        ps.setString(1, nombre);
+        ps.set
 
+
+    }
+
+    //MÉTODOS DE ANUNCIO
+    public void insertarAnuncio(String provincia, String localidad, Date fecha, Date hora, String direccion, String recorrido) {
+        Connection con=conectar();
+        String consulta = "INSERT INTO ANUNCIO(PROVINCIA, LOCALIDAD, FECHA, HORA, DIRECCIÓN, RECORRIDO) VALUES (?,?,?,?,?,?)";
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(consulta);
+            ps.setString(1, provincia);
+            ps.setString(2, localidad);
+            ps.setDate(3, fecha);
+            ps.setDate(4, hora);
+            ps.setString(5, direccion);
+            ps.setString(6, recorrido);
+            ps.close();
         } catch (SQLException e) {
             e.printStackTrace();
-            ps.close();
-            con.close();
         }
     }
 
+    //MÉTODOS DE MENSAJE
+    public void insertarMensaje(String asunto, String contenido, boolean estado) {
+        Connection con=conectar();
+        String consulta = "INSERT INTO MENSAJE(ASUNTO, CONTENIDO, ESTADO) VALUES (?,?,?)";
+        PreparedStatement ps = null;
+        try {
+            ps = con.prepareStatement(consulta);
+            ps.setString(1, asunto);
+            ps.setString(2, contenido);
+            ps.setBoolean(3, estado);
+            ps.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
