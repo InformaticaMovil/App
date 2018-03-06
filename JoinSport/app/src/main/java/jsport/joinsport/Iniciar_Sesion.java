4744c104016b32fb06d895b19461c5443453a94e
nerea1116 <36963103+nerea1116@users.noreply.github.com>
2018-03-06 18:33:16 +0100

    nerea Registro e Iniciar Sesion

---
+++ JoinSport/app/src/main/java/jsport/joinsport/Iniciar_Sesion.java
@@ -0,0 +1,13 @@
+package jsport.joinsport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Iniciar_Sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar__sesion);
+    }
+}
