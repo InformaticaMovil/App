c9b01c8ff4a16205492aa24a5cb5c8225b3cc6cf
nerea1116 <36963103+nerea1116@users.noreply.github.com>
2018-03-06 19:03:13 +0100

    nerea Registro e Iniciar Sesion

--- JoinSport/app/src/main/java/jsport/joinsport/Inicio.java
+++ JoinSport/app/src/main/java/jsport/joinsport/Inicio.java
@@ -1,7 +1,10 @@
 package jsport.joinsport;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
 
@@ -9,6 +12,24 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
-        //Ventana inicio APP12
+
+        final Button Registrarme = (Button) findViewById(R.id.btRegistro);
+        final Button IniciarSesion = (Button) findViewById(R.id.btLogin);
+
+        Registrarme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent CambiarActivity = new Intent(getApplicationContext(), Registro.class);
+                startActivity(CambiarActivity);
+            }
+        });
+
+        IniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent CambiarActivity2 = new Intent(getApplicationContext(), Iniciar_Sesion.class);
+                startActivity(CambiarActivity2);
+            }
+        });
     }
 }
