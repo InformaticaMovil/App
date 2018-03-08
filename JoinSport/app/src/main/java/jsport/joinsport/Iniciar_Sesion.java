c9461fbf3f0c54598494dbe24c056e7b0c03d40c
UNIRIOJA\paezqua <paulaezquerro@hotmail.com>
2018-03-08 17:23:00 +0100

    últimos cambios de Paula el 01/03

--- JoinSport/app/src/main/java/jsport/joinsport/Iniciar_Sesion.java
+++ JoinSport/app/src/main/java/jsport/joinsport/Iniciar_Sesion.java
@@ -1,7 +1,16 @@
 package jsport.joinsport;
 
+
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import Persistencia.Persistencia;
 
 public class Iniciar_Sesion extends AppCompatActivity {
 
@@ -9,5 +18,40 @@ public class Iniciar_Sesion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar__sesion);
+        final Button IniciarSesion = (Button) findViewById(R.id.btIniciarSesion);
+
+        IniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (datosValidos()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("Datos correctos");
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                    //Ir a la pantalla de los anuncios, no está creada todavía
+                    /*Intent CambiarActivity = new Intent(getApplicationContext(), Registro.class);
+                    startActivity(CambiarActivity);*/
+                } else {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage("Datos incorrectos");
+                    AlertDialog alert = builder.create();
+                    alert.show();
+                    final TextView textError = (TextView) findViewById(R.id.txError);
+                    textError.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
-}
+
+       public boolean datosValidos () {
+            final EditText valorEmail=(EditText) findViewById(R.id.txEmail);
+            String email=valorEmail.getText().toString();
+            final EditText valorPwd=(EditText) findViewById(R.id.txPwd);
+            String pwd=valorPwd.getText().toString();
+            String pwdEsperada=Persistencia.getPasswordUsuario(email);
+            return pwd.equals(pwdEsperada);
+        }
+
+    }
+
